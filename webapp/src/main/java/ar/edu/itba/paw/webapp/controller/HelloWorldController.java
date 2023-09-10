53809317f73058c37b0f4d3b7741888c374e7538
AdolfoRosauer1 <jurosauer@itba.edu.ar>
2023-09-10 18:55:35 -0300

    Changes by ian

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -29,6 +29,12 @@ public class HelloWorldController {
         return mav;
     }
 
+    @RequestMapping("/discover")
+    public ModelAndView discover() {
+        final ModelAndView mav = new ModelAndView("helloworld/discover");
+        return mav;
+    }
+
     @RequestMapping("/{id:\\d+}")
     public ModelAndView profile(@PathVariable("id") final long userId){
         final ModelAndView mav = new ModelAndView("helloworld/profile");
