ff9bef2f19b66fc4850013147235ae74a25804fc
MatiasWod <mwodtke@itba.edu.ar>
2023-09-11 01:45:38 -0300

    Added initial version of details.jsp and its access root in HelloWorldController.java

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class HelloWorldController {
 
@@ -54,5 +52,10 @@ public class HelloWorldController {
     @RequestMapping(value = "/register",method = RequestMethod.GET)
     public ModelAndView registerForm(){
         return new ModelAndView("helloworld/register");
+    }
+
+    @RequestMapping(value = "/details",method = RequestMethod.GET)
+    public ModelAndView details(){
+        return new ModelAndView("helloworld/details");
     }
 }
