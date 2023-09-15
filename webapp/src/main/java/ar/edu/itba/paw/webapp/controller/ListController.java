a19d2f0f58fe45ec508c14e176df146f69ab4207
AdolfoRosauer1 <jurosauer@itba.edu.ar>
2023-09-15 11:58:37 -0300

    small fix

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ListController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ListController.java
@@ -3,12 +3,15 @@ package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.models.Media.Media;
 import ar.edu.itba.paw.models.Media.Movie;
+import ar.edu.itba.paw.models.Media.TVSerie;
 import ar.edu.itba.paw.models.MoovieList.MoovieList;
+import ar.edu.itba.paw.services.GenreService;
 import ar.edu.itba.paw.services.MediaService;
 import ar.edu.itba.paw.services.MoovieListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -19,6 +22,9 @@ public class ListController {
 
     @Autowired
     private MoovieListService mediaListService;
+
+    @Autowired
+    private GenreService genreService;
 
     @Autowired
     private MediaService mediaService;
@@ -33,8 +39,15 @@ public class ListController {
 
 // http://tuDominio.com/createList?s=A&s=B&s=C&s=D&s=E
     @RequestMapping("/createList")
-    public ModelAndView createList(){
-        List<Media> media;
+    public ModelAndView createList(@RequestParam(value = "g", required = false) String genre,
+                                   @RequestParam(value = "m", required = false) String media,
+                                   @RequestParam(value = "q", required = false) String query){
+
+        List<Movie> movieList = null;
+        List<TVSerie> tvSerieList = null;
+        List<Media> mediaList = null;
+        List<Media> searchList;
+
         final ModelAndView mav = new ModelAndView("helloworld/createList");
 
         if (genre != null && !genre.isEmpty()) {
