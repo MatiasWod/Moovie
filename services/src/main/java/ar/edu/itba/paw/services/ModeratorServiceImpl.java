ee19cdbb65c8f2a93491d7dff974090fca15e81c
Juan Segundo Arnaude <juansarnaude@gmail.com>
2023-10-01 23:59:35 -0300

    Moderator Corrections

--- services/src/main/java/ar/edu/itba/paw/services/ModeratorServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/ModeratorServiceImpl.java
@@ -1,5 +1,7 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.exceptions.InvalidAuthenticationLevelRequiredToPerformActionException;
+import ar.edu.itba.paw.exceptions.UnableToBanUserException;
 import ar.edu.itba.paw.exceptions.UnableToChangeRoleException;
 import ar.edu.itba.paw.exceptions.UnableToFindUserException;
 import ar.edu.itba.paw.models.User.User;
@@ -7,8 +9,12 @@ import ar.edu.itba.paw.persistence.MoovieListDao;
 import ar.edu.itba.paw.persistence.ReviewDao;
 import ar.edu.itba.paw.persistence.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ModeratorServiceImpl implements ModeratorService{
+    @Autowired
+    private UserService userService;
     @Autowired
     private ReviewDao reviewDao;
     @Autowired
@@ -19,25 +25,40 @@ public class ModeratorServiceImpl implements ModeratorService{
 
     @Override
     public void deleteReview(int reviewId) {
+        amIModerator();
         reviewDao.deleteReview(reviewId);
     }
 
     @Override
     public void deleteMoovieListList(int moovieListId) {
+        amIModerator();
         moovieListDao.deleteMoovieList(moovieListId);
     }
 
     @Override
     public void banUser(int userId) {
+        amIModerator();
+        User u = userDao.findUserById(userId).orElseThrow(() -> new UnableToFindUserException("No user for the id = " + userId ));
+        if(u.getRole() == userDao.ROLE_MODERATOR){
+            throw new UnableToBanUserException("Cannot ban another moderator");
+        }
         userDao.changeUserRole(userId, userDao.ROLE_BANNED);
     }
 
     @Override
     public void makeUserModerator(int userId) {
+        amIModerator();
         User u = userDao.findUserById(userId).orElseThrow(() -> new UnableToFindUserException("No user for the id = " + userId ));
         if(u.getRole() != userDao.ROLE_USER ){
             throw new UnableToChangeRoleException("Unable to change role of uid: " + userId + ", user must be ROLE_USER");
         }
         userDao.changeUserRole(userId, userDao.ROLE_MODERATOR);
     }
+
+    private void amIModerator(){
+        if(userService.getInfoOfMyUser().getRole() == 2){
+            throw new InvalidAuthenticationLevelRequiredToPerformActionException("To perform this action you must have role: moderator");
+        }
+    }
+
 }
