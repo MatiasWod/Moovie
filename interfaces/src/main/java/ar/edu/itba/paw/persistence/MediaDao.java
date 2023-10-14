07b050658328a97191e7ab423e84d335b10821f6
AdolfoRosauer1 <jurosauer@itba.edu.ar>
2023-10-14 18:38:27 -0300

    fixed merge

--- interfaces/src/main/java/ar/edu/itba/paw/persistence/MediaDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/persistence/MediaDao.java
@@ -17,8 +17,4 @@ public interface MediaDao {
     Optional<TVSerie> getTvById(int mediaId);
     int getMediaCount(int type, String search, String participantSearch, List<String> genres, List<String> providers);
 
-    void upMediaVoteCount(int mediaId);
-
-    void downMediaVoteCount(int mediaId);
-    int getMediaCount(int type, String search, List<String> genres);
 }
