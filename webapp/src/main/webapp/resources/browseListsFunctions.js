1909a431716383f161e759d2c1ab09ebcc7881b3
AdolfoRosauer1 <jurosauer@itba.edu.ar>
2023-10-15 03:33:40 -0300

    fixes to sort and order labels!

--- webapp/src/main/webapp/resources/browseListsFunctions.js
+++ webapp/src/main/webapp/resources/browseListsFunctions.js
@@ -5,10 +5,10 @@ function changeSortOrder(sortOrderInputId, sortIconId) {
     var sortOrderInput = document.getElementById(sortOrderInputId);
 
     if (sortOrderInput.value === 'asc') {
-        sortIcon.className = 'bi bi-arrow-up-circle-fill';
+        sortIcon.className = 'bi bi-arrow-down-circle-fill';
         sortOrderInput.value = 'desc'; // Toggle sortOrder to 'desc' when changing to ascending
     } else {
-        sortIcon.className = 'bi bi-arrow-down-circle-fill';
+        sortIcon.className = 'bi bi-arrow-up-circle-fill';
         sortOrderInput.value = 'asc'; // Toggle sortOrder to 'asc' when changing to descending
     }
 
