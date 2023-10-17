4e5f729c04b5085ef010f277a9724fc7a999cef8
Juan Segundo Arnaude <juansarnaude@gmail.com>
2023-10-17 20:05:38 -0300

    Removed unnescesary dependencies

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/BannedUserController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/BannedUserController.java
@@ -1,10 +1,8 @@
 package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.services.BannedService;
-import ar.edu.itba.paw.webapp.form.RegisterForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
