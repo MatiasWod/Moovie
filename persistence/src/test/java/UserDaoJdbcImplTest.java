e8c6991552dc4cbd4912e55387cac19876c6c7c3
Juan Segundo Arnaude <juansarnaude@gmail.com>
2023-10-17 03:28:33 -0300

    testing started

--- persistence/src/test/java/UserDaoJdbcImplTest.java
+++ persistence/src/test/java/UserDaoJdbcImplTest.java
@@ -1,45 +1,59 @@
-//import ar.edu.itba.paw.models.User.User;
-//import ar.edu.itba.paw.persistence.UserDaoJdbcImpl;
-//import config.TestConfig;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.jdbc.Sql;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.jdbc.JdbcTestUtils;
-//
-//import javax.sql.DataSource;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertNotNull;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = TestConfig.class)
-//public class UserDaoJdbcImplTest {
-//    private static final String EMAIL = "asd@email.com";
-//
-//    @Autowired
-//    private DataSource ds;
-//
-//    @Autowired
-//    private UserDaoJdbcImpl userDaoJdbc;
-//
-//    private JdbcTemplate jdbcTemplate;
-//
-//    @Before
-//    public void setUp(){
-//        jdbcTemplate = new JdbcTemplate(ds);
-//        JdbcTestUtils.deleteFromTables(jdbcTemplate,"users");
-//    }
-//
-//    @Test
-//    public void testCreate(){
-//        final User user = userDaoJdbc.createUser(EMAIL);
-//        assertNotNull(user);
-//        assertEquals(EMAIL,user.getEmail());
-//        assertEquals(1,JdbcTestUtils.countRowsInTable(jdbcTemplate,"users"));
-//    }
-//}
+import ar.edu.itba.paw.models.User.User;
+import ar.edu.itba.paw.persistence.UserDaoJdbcImpl;
+import config.TestConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+public class UserDaoJdbcImplTest {
+
+    private static final int EXISTING_USER_ID = 1;
+    private static final String EXISTING_USERNAME = "Cavani";
+    private static final String EXISTING_EMAIL = "cavani@test.com";
+
+    private static final String USERNAME = "SteveAoki";
+    private static final String EMAIL = "asd@email.com";
+    private static final String PASSWORD = "pass123";
+
+    @Autowired
+    private DataSource ds;
+
+    @Autowired
+    private UserDaoJdbcImpl userDaoJdbc;
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Before
+    public void setUp(){
+        jdbcTemplate = new JdbcTemplate(ds);
+    }
+
+    @Test
+    public void testCreate(){
+        final User user = userDaoJdbc.createUser(USERNAME, EMAIL, PASSWORD);
+        assertNotNull(user);
+        assertEquals(EMAIL,user.getEmail());
+    }
+
+    @Test
+    public void testGetUser(){
+        final Optional<User> user = userDaoJdbc.findUserById(EXISTING_USER_ID);
+
+        assertTrue(user.isPresent());
+        assertEquals(user.get().getUsername(), EXISTING_USERNAME);
+        assertEquals(user.get().getEmail(), EXISTING_EMAIL);
+    }
+
+}
