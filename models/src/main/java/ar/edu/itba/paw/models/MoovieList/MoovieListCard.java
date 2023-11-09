821b3f2beb57e550ec6a5eae014b125d19ce9934
Juan Segundo Arnaude <juansarnaude@gmail.com>
2023-11-09 18:09:25 -0300

    El DAO ahora es LOCURA

--- models/src/main/java/ar/edu/itba/paw/models/MoovieList/MoovieListCard.java
+++ models/src/main/java/ar/edu/itba/paw/models/MoovieList/MoovieListCard.java
@@ -3,26 +3,202 @@ package ar.edu.itba.paw.models.MoovieList;
 import org.hibernate.annotations.Formula;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class MoovieListCard extends MoovieListCardEntity{
+@Entity
+@Table(name="moovielists")
+public class MoovieListCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "moovielists_moovielistid_seq")
+    @SequenceGenerator(sequenceName = "moovielists_moovielistid_seq", name = "moovielists_moovielistid_seq", allocationSize = 1)
+    @Column(name = "moovielistId")
+    private int moovieListId;
 
-    MoovieListCardUserStatus mlcUS;
-    public MoovieListCard(MoovieListCardEntity mlcE , MoovieListCardUserStatus mlcUS ){
-        super(mlcE.getMoovieListId(), mlcE.getName(), mlcE.getUsername(), mlcE.getDescription(), mlcE.getLikeCount(), mlcE.getFollowerCount(), mlcE.getType(), mlcE.getSize(), mlcE.getMoviesAmount(), mlcE.getImages());
-        this.mlcUS = mlcUS;
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = true, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private int type;
+
+    @Formula("(SELECT users.username FROM users WHERE users.userid = userid)")
+    private String username;
+
+    @Formula("(SELECT COUNT(*) FROM moovielistslikes mll WHERE mll.moovielistid = moovielistid)")
+    private int likeCount;
+
+    @Formula("(SELECT COUNT(*) FROM moovielistsfollows mll WHERE mll.moovielistid = moovielistid)")
+    private int followerCount;
+
+    @Formula("(SELECT COUNT(*) FROM moovieListsContent mlc WHERE mlc.moovieListId = moovieListId)")
+    private int size;
+
+    @Formula("(SELECT COUNT(*) FROM moovielistsContent mlc INNER JOIN media m ON mlc.mediaid = m.mediaid WHERE m.type = false AND mlc.moovieListid = moovieListId)")
+    private int moviesAmount;
+
+    @Formula("( SELECT ARRAY_AGG(m.posterPath) FROM moovielistscontent mlc INNER JOIN media m ON mlc.mediaId = m.mediaId WHERE mlc.moovielistId = moovieListId LIMIT 4 )")
+    private String images;
+
+    @Transient
+    private int currentUserWatchAmount;
+
+    @Transient
+    private boolean currentUserHasLiked;
+
+    @Transient
+    private boolean currentUserHasFollowed;
+
+    public MoovieListCard(){}
+
+    public MoovieListCard(int moovieListId, String name, String username, String description, int likeCount, int followerCount, int type, int size, int moviesAmount, String images) {
+        this.moovieListId = moovieListId;
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.username = username;
+        this.likeCount = likeCount;
+        this.followerCount = followerCount;
+        this.size = size;
+        this.moviesAmount = moviesAmount;
+        this.images = images;
+    }
+
+    public MoovieListCard(int moovieListId, String name, String username, String description, int likeCount, int followerCount, int type, int size, int moviesAmount,
+                          String images, int currentUserWatchAmount, boolean currentUserHasLiked, boolean currentUserHasFollowed) {
+        this.moovieListId = moovieListId;
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.username = username;
+        this.likeCount = likeCount;
+        this.followerCount = followerCount;
+        this.size = size;
+        this.moviesAmount = moviesAmount;
+        this.images = images;
+        this.currentUserWatchAmount = currentUserWatchAmount;
+        this.currentUserHasLiked = currentUserHasLiked;
+        this.currentUserHasFollowed = currentUserHasFollowed;
+    }
+
+
+    public int getMoovieListId() {
+        return moovieListId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public int getFollowerCount() {
+        return followerCount;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getMoviesAmount() {
+        return moviesAmount;
+    }
+
+    public List<String> getImages() {
+        List<String> toRet = new ArrayList<>();
+        if(this.images!=null){
+            String[] aux = this.images.replaceAll("[{}]","").split(",");
+            toRet = new ArrayList<>(Arrays.asList(aux));
+        }else{
+            toRet = new ArrayList<>();
+        }
+        return toRet;
     }
 
     public int getCurrentUserWatchAmount() {
-        return mlcUS.getCurrentUserWatchAmount();
+        return currentUserWatchAmount;
     }
 
     public boolean isCurrentUserHasLiked() {
-        return mlcUS.isCurrentUserHasLiked();
+        return currentUserHasLiked;
     }
 
     public boolean isCurrentUserHasFollowed() {
-        return mlcUS.isCurrentUserHasFollowed();
+        return currentUserHasFollowed;
     }
 
+    public void setMoovieListId(int moovieListId) {
+        this.moovieListId = moovieListId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public void setFollowerCount(int followerCount) {
+        this.followerCount = followerCount;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setMoviesAmount(int moviesAmount) {
+        this.moviesAmount = moviesAmount;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+
+    public void setUserStatus(int currentUserWatchAmount, boolean currentUserHasLiked, boolean currentUserHasFollowed){
+        this.currentUserWatchAmount = currentUserWatchAmount;
+        this.currentUserHasLiked = currentUserHasLiked;
+        this.currentUserHasFollowed = currentUserHasFollowed;
+    }
+
+    public void setCurrentUserWatchAmount(int currentUserWatchAmount) {
+        this.currentUserWatchAmount = currentUserWatchAmount;
+    }
+
+    public void setCurrentUserHasLiked(boolean currentUserHasLiked) {
+        this.currentUserHasLiked = currentUserHasLiked;
+    }
+
+    public void setCurrentUserHasFollowed(boolean currentUserHasFollowed) {
+        this.currentUserHasFollowed = currentUserHasFollowed;
+    }
 }
--- models/src/main/java/ar/edu/itba/paw/models/MoovieList/MoovieListContent.java
+++ models/src/main/java/ar/edu/itba/paw/models/MoovieList/MoovieListContent.java
@@ -4,39 +4,63 @@ import ar.edu.itba.paw.models.Genre.Genre;
 import ar.edu.itba.paw.models.Media.Media;
 import ar.edu.itba.paw.models.Provider.Provider;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
 
+@Entity
+@Table(name = "moovielistscontent")
+@PrimaryKeyJoinColumn(name = "mediaId")
+public class MoovieListContent extends Media {
 
-public class MoovieListContent extends MoovieListContentEntity {
+    @Column
+    private int moovieListId;
 
+    @Column
+    private int customOrder;
+
+    @Transient
     private boolean watched;
 
-    public MoovieListContent(MoovieListContentEntity mlc,
-                             boolean watched) {
-        super(mlc.getMediaId(), mlc.isType(), mlc.getName(), mlc.getOriginalLanguage(),
-                mlc.isAdult(), mlc.getReleaseDate(), mlc.getOverview(), mlc.getBackdropPath(),
-                mlc.getPosterPath(), mlc.getTrailerLink(), mlc.getTmdbRating(), mlc.getTotalRating(),
-                mlc.getVoteCount(), mlc.getStatus(), mlc.getGenresModels(), mlc.getProviders(),
-                mlc.getMoovieListId(), mlc.getCustomOrder());
-        this.watched = watched;
+    MoovieListContent(){}
+
+    public MoovieListContent(int mediaId, boolean type, String name, String originalLanguage,
+                                   boolean adult, Date releaseDate, String overview, String backdropPath,
+                                   String posterPath, String trailerLink, float tmdbRating, float totalRating,
+                                   int voteCount, String status, List<Genre> genres, List<Provider> providers,
+                                   int moovieListId, int customOrder) {
+        super(mediaId, type, name, originalLanguage, adult, releaseDate, overview, backdropPath, posterPath, trailerLink, tmdbRating, totalRating, voteCount, status, genres, providers);
+        this.moovieListId = moovieListId;
+        this.customOrder = customOrder;
     }
 
     public MoovieListContent(Media media, int moovieListId, int customOrder, boolean watched){
         super(media.getMediaId(), media.isType(), media.getName(), media.getOriginalLanguage(),
                 media.isAdult(), media.getReleaseDate(), media.getOverview(), media.getBackdropPath(),
                 media.getPosterPath(), media.getTrailerLink(), media.getTmdbRating(), media.getTotalRating(),
-                media.getVoteCount(), media.getStatus(), media.getGenresModels(), media.getProviders(),
-                moovieListId, customOrder);
+                media.getVoteCount(), media.getStatus(), media.getGenresModels(), media.getProviders());
         this.watched = watched;
     }
 
-   MoovieListContent(){}
+    public MoovieListContent(MoovieListContent mlc,
+                             boolean watched) {
+        super(mlc.getMediaId(), mlc.isType(), mlc.getName(), mlc.getOriginalLanguage(),
+                mlc.isAdult(), mlc.getReleaseDate(), mlc.getOverview(), mlc.getBackdropPath(),
+                mlc.getPosterPath(), mlc.getTrailerLink(), mlc.getTmdbRating(), mlc.getTotalRating(),
+                mlc.getVoteCount(), mlc.getStatus(), mlc.getGenresModels(), mlc.getProviders());
+        this.customOrder = mlc.customOrder;
+        this.moovieListId = mlc.moovieListId;
+        this.watched = watched;
+    }
 
+
+    public int getCustomOrder() {
+        return customOrder;
+    }
+
+    public int getMoovieListId() {
+        return moovieListId;
+    }
 
     public boolean isWatched() {
         return watched;
@@ -44,5 +68,13 @@ public class MoovieListContent extends MoovieListContentEntity {
 
     public void setWatched(boolean watched) {
         this.watched = watched;
+    }
+
+    public void setMoovieListId(int moovieListId) {
+        this.moovieListId = moovieListId;
+    }
+
+    public void setCustomOrder(int customOrder) {
+        this.customOrder = customOrder;
     }
 }
