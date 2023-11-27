d9d3225d5b69d9280b4d22c1117cf5d8a8b6d1f2
Juan Segundo Arnaude <juansarnaude@gmail.com>
2023-11-26 21:05:50 -0300

    small fix

--- persistence/src/main/resources/featured_lists.sql
+++ persistence/src/main/resources/featured_lists.sql
@@ -1,4 +1,4 @@
-INSERT INTO users(userid, email, role, username, password) VALUES ( 21 , 'pawmoovie@gmail.com', 1,' Moovie', '$2a$10$ikToc9ArDQyT6IYnixyJpeiVoIEDD9IbrI390wZmBTcU1AyI107IC') ON CONFLICT DO NOTHING;
+INSERT INTO users(userid, email, role, username, password) VALUES ( 21 , 'pawmoovie@gmail.com', 1,'Moovie', '$2a$10$ikToc9ArDQyT6IYnixyJpeiVoIEDD9IbrI390wZmBTcU1AyI107IC') ON CONFLICT DO NOTHING;
 
 INSERT INTO moovielists(moovielistid, userid,name,description,type) VALUES(40, 21,'Top Rated Movies','These are the best rated movies according to our users!',3) ON CONFLICT DO NOTHING;
 INSERT INTO moovielists(moovielistid, userid,name,description,type) VALUES(41, 21,'Top Rated TV Series','These are the best rated series according to our users!',3) ON CONFLICT DO NOTHING;
