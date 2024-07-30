7f2818bb1117d04f9174562e1376f1d613aa8551
MatiasWod <mwodtke@itba.edu.ar>
2024-07-30 20:35:51 -0300

    Added bootstrap-icons to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from "./App";
 import './i18n'
 
