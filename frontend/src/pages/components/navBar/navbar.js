ed952bd00fbbdef76d3dc26fb601f9832fb57c39
MatiasWod <mwodtke@itba.edu.ar>
2024-08-04 18:57:18 -0300

    Better style for navbar

--- frontend/src/pages/components/navBar/navbar.js
+++ frontend/src/pages/components/navBar/navbar.js
@@ -7,47 +7,47 @@ import { Image, InputGroup } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Logo from '../../../images/logo.png';
 import NavbarStyle from './navbarStyle.css';
+import {NavLink} from "react-router-dom";
 
 function NavbarComponent() {
     return (
-
         <Navbar expand="lg" className="sticky-top navbar navbar-expand-lg navbar-light container-nav mb-4">
             <Container fluid>
                 <Navbar.Brand href="/" className="d-flex align-items-center">
-                    <Image src={Logo} width={60} className="me-2"/> Moovie
+                    <Image src={Logo} width={50} className="me-2" /> Moovie
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link activeClassName="active" href="discover">Discover</Nav.Link>
-                        <Nav.Link activeClassName="active" href="moovieLists">Browse Lists</Nav.Link>
+                        <Nav.Link as={NavLink} to="/discover" activeClassName="active">Discover</Nav.Link>
+                        <Nav.Link as={NavLink} to="/moovieLists" activeClassName="active">Browse Lists</Nav.Link>
                         <NavDropdown title="Top Rated" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Media</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Movies</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Series</NavDropdown.Item>
+                            <NavDropdown.Item to="#action/3.1" activeClassName="active">Media</NavDropdown.Item>
+                            <NavDropdown.Item to="#action/3.2" activeClassName="active">Movies</NavDropdown.Item>
+                            <NavDropdown.Item to="#action/3.3" activeClassName="active">Series</NavDropdown.Item>
                         </NavDropdown>
-                        <NavDropdown title="Top Rated" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Media</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Movies</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Series</NavDropdown.Item>
+                        <NavDropdown title="Most Popular" id="basic-nav-dropdown">
+                            <NavDropdown.Item to="#action/3.1" activeClassName="active">Media</NavDropdown.Item>
+                            <NavDropdown.Item to="#action/3.2" activeClassName="active">Movies</NavDropdown.Item>
+                            <NavDropdown.Item to="#action/3.3" activeClassName="active">Series</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link activeClassName="active" href="leaderboard">Leaderboard</Nav.Link>
+                        <Nav.Link as={NavLink} to="/leaderboard" activeClassName="active">Leaderboard</Nav.Link>
                     </Nav>
                     <Form inline>
                         <InputGroup className={"InputGroup"}>
-                                <Form.Control
-                                    type="search"
-                                    placeholder="Search"
-                                    className="me-2"
-                                />
-                                <Button variant="outline-success" type="submit">
-                                    <i className={"bi bi-search"}></i>
-                                    Search
-                                </Button>
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                            />
+                            <Button variant="outline-success" type="submit">
+                                <i className={"bi bi-search"}></i>
+                                Search
+                            </Button>
                         </InputGroup>
                     </Form>
                     <Nav className="d-flex nav-item justify-content-center userPic-login">
-                        <Nav.Link href="login">Login</Nav.Link>
+                        <Nav.Link as={NavLink} to="/login" activeClassName="active" className={'link-primary'}>Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
