cd88a4be228cf4e045458f0979282ceb7c0fceaf
Matias Wodtke <86923541+MatiasWod@users.noreply.github.com>
2024-08-12 18:42:24 -0300

    Added get reviews by mediaId

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ReviewController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ReviewController.java
@@ -1,22 +1,20 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.models.PagingSizes;
 import ar.edu.itba.paw.models.Review.Review;
 import ar.edu.itba.paw.services.ReviewService;
+import ar.edu.itba.paw.webapp.dto.MediaDto;
 import ar.edu.itba.paw.webapp.dto.ReviewDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.util.List;
 //TODO CHECK LOGGERS
 //import com.sun.org.slf4j.internal.Logger;
 //import com.sun.org.slf4j.internal.LoggerFactory;
+
 
 @Path("review")
 @Component
@@ -44,6 +42,22 @@ public class ReviewController {
             }
             final ReviewDto reviewDto = ReviewDto.fromReview(review, uriInfo);
             return Response.ok(reviewDto).build();
+        } catch (RuntimeException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+    }
+
+    @GET
+    @Path("/media/{mediaId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getReviewsByMediaId(@PathParam("mediaId") final int mediaId, @QueryParam("pageNumber") @DefaultValue("1") final int page) {
+        try {
+            final List<Review> reviews = reviewService.getReviewsByMediaId(mediaId, PagingSizes.REVIEW_DEFAULT_PAGE_SIZE.getSize(), page-1);
+            if (reviews.isEmpty() && page == 1) {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+            final List<ReviewDto> reviewDtos = ReviewDto.fromReviewList(reviews, uriInfo);
+            return Response.ok(new GenericEntity<List<ReviewDto>>(reviewDtos) {}).build();
         } catch (RuntimeException e) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
         }
