69719b176b49dc1b324b00b0d076932e7c879d34
bcanevaro <bcanevaro@itba.edu.ar>
2024-11-29 20:34:49 -0300

    Added fix to pagination on browseLists.

--- frontend/src/pages/components/dropdownMenu/DropdownMenu.js
+++ frontend/src/pages/components/dropdownMenu/DropdownMenu.js
@@ -1,33 +1,40 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import OrderBy from "../../../api/values/MediaOrderBy";
 import SortOrder from "../../../api/values/SortOrder";
 import Button from "react-bootstrap/Button";
 
+const DropdownMenu = ({ setOrderBy, setSortOrder, currentSortOrder, values }) => {
+    const [btnState, setBtnState] = useState(currentSortOrder);
 
-const DropdownMenu = ({setOrderBy, setSortOrder, currentSortOrder, values}) => {
+    useEffect(() => {
+        setBtnState(currentSortOrder);
+    }, [currentSortOrder]);
 
-    const handleSelect = (selectedValue) =>{
+    const handleSelect = (selectedValue) => {
         setOrderBy(selectedValue);
     };
 
-    const [btnState, setBtnState] = useState(currentSortOrder);
-    const handleClick = () =>{
-        setBtnState( (btnState===SortOrder.DESC) ? SortOrder.ASC : SortOrder.DESC );
-        setSortOrder( btnState );
-
-    }
+    const handleClick = () => {
+        const newSortOrder = btnState === SortOrder.DESC ? SortOrder.ASC : SortOrder.DESC;
+        setBtnState(newSortOrder);
+        setSortOrder(newSortOrder);
+    };
 
     return (
-        <div style={{display: "flex"}}>
+        <div style={{ display: "flex" }}>
             <NavDropdown title="Order By">
                 {values.map((value) => (
-                    <NavDropdown.Item onClick={ ()=>handleSelect(value)}>{value}</NavDropdown.Item>
+                    <NavDropdown.Item key={value} onClick={() => handleSelect(value)}>
+                        {value}
+                    </NavDropdown.Item>
                 ))}
             </NavDropdown>
-            <Button onClick={handleClick}>{(btnState===SortOrder.DESC) ? '↑' : '↓' }</Button>
+            <Button onClick={handleClick}>
+                {btnState === SortOrder.DESC ? "↑" : "↓"}
+            </Button>
         </div>
     );
-}
+};
 
 export default DropdownMenu;
