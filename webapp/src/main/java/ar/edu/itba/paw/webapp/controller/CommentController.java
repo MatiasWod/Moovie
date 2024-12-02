86f6608f7a1b532a14c349240479a473be060474
MatiasWod <mwodtke@itba.edu.ar>
2024-12-01 23:27:26 -0300

    Comment controller

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/CommentController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/CommentController.java
@@ -1,4 +1,122 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.exceptions.ReviewNotFoundException;
+import ar.edu.itba.paw.exceptions.UserNotLoggedException;
+import ar.edu.itba.paw.models.Comments.Comment;
+import ar.edu.itba.paw.services.CommentService;
+import ar.edu.itba.paw.webapp.dto.out.CommentDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@Path("comment")
+@Component
 public class CommentController {
+    private final CommentService commentService;
+
+    @Context
+    UriInfo uriInfo;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCommentById(@PathParam("id") int id) {
+        try {
+            final Comment comment = commentService.getCommentById(id);
+            final CommentDto commentDto = CommentDto.fromComment(comment,uriInfo);
+            return Response.ok(commentDto).build();
+        }catch (RuntimeException e) {
+            return Response.serverError().entity(e.getMessage()).build();
+        }
+    }
+
+    @POST
+    @Path("/{id}/like")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response likeComment(@PathParam("id") int id) {
+        try {
+            boolean liked = commentService.likeComment(id);
+            if (liked){
+                return Response.ok()
+                        .entity("Comment liked").build();
+            }
+            return Response.ok().entity("Comment not liked").build();
+        }catch(UserNotLoggedException e){
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("{\"error\":\"User must be logged in to like a comment.\"}")
+                    .build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("An unexpected error occurred: " + e.getMessage())
+                    .build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteComment(@PathParam("id") int id) {
+        try {
+            commentService.deleteComment(id);
+            return Response.ok().entity("Comment deleted").build();
+        }catch(UserNotLoggedException e){
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("{\"error\":\"User must be logged in to delete a comment.\"}")
+                    .build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("An unexpected error occurred: " + e.getMessage())
+                    .build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}/removeLikeComment")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response removeLikeComment(@PathParam("id") int id) {
+        try {
+            commentService.removeLikeComment(id);
+            return Response.ok().entity("Comment like removed").build();
+        }catch(UserNotLoggedException e){
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("{\"error\":\"User must be logged to unlike a comment.\"}")
+                    .build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("An unexpected error occurred: " + e.getMessage())
+                    .build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}/removeUnlikeComment")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response removeUnlikeComment(@PathParam("id") int id) {
+        try {
+            commentService.removeDislikeComment(id);
+            return Response.ok().entity("Comment dislike removed").build();
+        }catch(UserNotLoggedException e){
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("{\"error\":\"User must be logged in to undislike a comment.\"}")
+                    .build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("An unexpected error occurred: " + e.getMessage())
+                    .build();
+        }
+    }
+
+
+
+
 }
--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ReviewController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ReviewController.java
@@ -1,20 +1,27 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.exceptions.ReviewAlreadyCreatedException;
 import ar.edu.itba.paw.exceptions.ReviewNotFoundException;
 import ar.edu.itba.paw.exceptions.UnableToFindUserException;
 import ar.edu.itba.paw.exceptions.UserNotLoggedException;
+import ar.edu.itba.paw.models.Comments.Comment;
+import ar.edu.itba.paw.models.PagingSizes;
+import ar.edu.itba.paw.models.PagingUtils;
 import ar.edu.itba.paw.models.Review.Review;
 import ar.edu.itba.paw.models.Review.ReviewTypes;
+import ar.edu.itba.paw.services.CommentService;
 import ar.edu.itba.paw.services.ReviewService;
+import ar.edu.itba.paw.webapp.dto.in.CommentCreateDto;
+import ar.edu.itba.paw.webapp.dto.out.CommentDto;
 import ar.edu.itba.paw.webapp.dto.out.ReviewDto;
+import ar.edu.itba.paw.webapp.utils.ResponseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.validation.Valid;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.*;
+import java.util.List;
 //TODO CHECK LOGGERS
 //import com.sun.org.slf4j.internal.Logger;
 //import com.sun.org.slf4j.internal.LoggerFactory;
@@ -24,6 +31,7 @@ import javax.ws.rs.core.UriInfo;
 @Component
 public class ReviewController {
     private final ReviewService reviewService;
+    private final CommentService commentService;
 
     @Context
     UriInfo uriInfo;
@@ -31,8 +39,9 @@ public class ReviewController {
     //private static final Logger LOGGER = LoggerFactory.getLogger(ReviewController.class);
 
     @Autowired
-    public ReviewController(ReviewService reviewService) {
+    public ReviewController(ReviewService reviewService, CommentService commentService) {
         this.reviewService = reviewService;
+        this.commentService=commentService;
     }
 
 
@@ -109,6 +118,58 @@ public class ReviewController {
         }
     }
 
+    /* COMMENTS */
+    @GET
+    @Path("/{id}/comments")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getComments(@PathParam("id") final int id, @QueryParam("pageNumber") @DefaultValue("1") final int page) {
+        try{
+            final int commentCount=reviewService.getReviewById(id).getCommentCount().intValue();
+            final List<Comment> commentList= commentService.getComments(id, PagingSizes.REVIEW_DEFAULT_PAGE_SIZE.getSize(),page);
+            final List<CommentDto> commentDtoList=CommentDto.fromCommentList(commentList,uriInfo);
+
+            Response.ResponseBuilder res = Response.ok(new GenericEntity<List<CommentDto>>(commentDtoList) {});
+            final PagingUtils<Comment> reviewPagingUtils = new PagingUtils<>(commentList,page,PagingSizes.REVIEW_DEFAULT_PAGE_SIZE.getSize(),commentCount);
+            ResponseUtils.setPaginationLinks(res,reviewPagingUtils,uriInfo);
+            return res.build();
+        } catch(ReviewNotFoundException e){
+            return Response.status(Response.Status.CONFLICT)
+                    .entity("{\"error\":\"Review doesn't exists.\"}")
+                    .build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("An unexpected error occurred: " + e.getMessage())
+                    .build();
+        }
+    }
+
+    @POST
+    @Path("/{id}/comment")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createComment(@PathParam("id") final int id,@Valid final CommentCreateDto commentDto){
+        try {
+            commentService.createComment(
+                    id,
+                    commentDto.getCommentContent()
+            );
+            return Response.status(Response.Status.CREATED)
+                    .entity("Comment successfully created to review with id:" + id)
+                    .build();
+        }catch(UserNotLoggedException e){
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("{\"error\":\"User must be logged in to create a comment.\"}")
+                    .build();
+        } catch(ReviewNotFoundException e){
+            return Response.status(Response.Status.CONFLICT)
+                    .entity("{\"error\":\"Review doesn't exists.\"}")
+                    .build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("An unexpected error occurred: " + e.getMessage())
+                    .build();
+        }
+    }
 
 }
 
