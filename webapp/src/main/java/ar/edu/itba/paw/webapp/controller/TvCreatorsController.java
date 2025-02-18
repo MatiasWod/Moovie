286d37a59249db333e00111553460e6576f6623b
MatiasWod <mwodtke@itba.edu.ar>
2025-02-18 01:28:49 -0300

    Better Tv Creators Controller

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/TvCreatorsController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/TvCreatorsController.java
@@ -33,32 +33,48 @@ public class TvCreatorsController {
             @QueryParam("search") final String search,
             @QueryParam("mediaId") final Integer mediaId
     ) {
-        if (search != null && !search.isEmpty()) {
-            // Lógica para obtener creadores de TV por consulta de búsqueda
-            List<TVCreators> tvCreatorsList = tvCreatorsService.getTVCreatorsForQuery(search, 10);
-            List<TvCreatorsDto> tvCreatorsDtoList = TvCreatorsDto.fromTvCreatorList(tvCreatorsList, uriInfo);
-            return Response.ok(new GenericEntity<List<TvCreatorsDto>>(tvCreatorsDtoList) {}).build();
-        } else if (mediaId != null) {
-            // Lógica para obtener creadores de TV por ID de medio
-            List<TVCreators> tvCreators = tvCreatorsService.getTvCreatorsByMediaId(mediaId);
-            List<TvCreatorsDto> tvCreatorsDtos = TvCreatorsDto.fromTvCreatorList(tvCreators, uriInfo);
-            return Response.ok(new GenericEntity<List<TvCreatorsDto>>(tvCreatorsDtos) {}).build();
+        try {
+            if (search != null && !search.isEmpty()) {
+                // Lógica para obtener creadores de TV por consulta de búsqueda
+                List<TVCreators> tvCreatorsList = tvCreatorsService.getTVCreatorsForQuery(search, 10);
+                List<TvCreatorsDto> tvCreatorsDtoList = TvCreatorsDto.fromTvCreatorList(tvCreatorsList, uriInfo);
+                return Response.ok(new GenericEntity<List<TvCreatorsDto>>(tvCreatorsDtoList) {}).build();
+            } else if (mediaId != null) {
+                // Lógica para obtener creadores de TV por ID de medio
+                List<TVCreators> tvCreators = tvCreatorsService.getTvCreatorsByMediaId(mediaId);
+                List<TvCreatorsDto> tvCreatorsDtos = TvCreatorsDto.fromTvCreatorList(tvCreators, uriInfo);
+                return Response.ok(new GenericEntity<List<TvCreatorsDto>>(tvCreatorsDtos) {}).build();
+            }
+
+            // Si no se proporcionan parámetros válidos
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("You must provide either 'search' or 'mediaId' as query parameters.")
+                    .build();
+        }catch (Exception e){
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("An error occurred while processing the request.")
+                    .build();
         }
 
-        // Si no se proporcionan parámetros válidos
-        return Response.status(Response.Status.BAD_REQUEST)
-                .entity("You must provide either 'search' or 'mediaId' as query parameters.")
-                .build();
     }
 
     @GET
     @Path("/{id}")
     @Produces(VndType.APPLICATION_TVCREATOR)
     public Response getTvCreatorById(@PathParam("id") final int tvCreatorId) {
-        TVCreators tvCreators=tvCreatorsService.getTvCreatorById(tvCreatorId);
-        return Response.ok(TvCreatorsDto.fromTvCreator(tvCreators,uriInfo)).build();
+        try {
+            TVCreators tvCreators=tvCreatorsService.getTvCreatorById(tvCreatorId);
+            if (tvCreators==null){
+                return Response.status(Response.Status.NOT_FOUND)
+                        .entity("TvCreator not found.")
+                        .build();
+            }
+            return Response.ok(TvCreatorsDto.fromTvCreator(tvCreators,uriInfo)).build();
+        } catch (Exception e){
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("An error occurred while processing the request.")
+                    .build();
+        }
     }
-
-
 }
 
